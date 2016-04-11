44514de1dbd388e904ccc777d636500b0218d808
thumbimigwe <thumbimigwe@gmail.com>
2016-04-11 22:04:22 +0300

    added categories on the todo app on the admin panel

--- src/todoListApp/admin.py
+++ src/todoListApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from.models import toDo
+
 # Register your models here.
+
+class toDoAdmin(admin.ModelAdmin):
+    list_display = ("description", "done")
+
+admin.site.register(toDo, toDoAdmin)
