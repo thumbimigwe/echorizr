68ba6a8e291a17f2bad1ca8095a403b5cd18cf35
thumbimigwe <thumbimigwe@gmail.com>
2016-04-11 21:57:40 +0300

    models

--- src/todoListApp/models.py
+++ src/todoListApp/models.py
@@ -1,5 +1,9 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
+class toDo(models.Model):
+    
+    description = models.CharField(max_length=225)
+    done = models.BooleanField(default=False)
+    
