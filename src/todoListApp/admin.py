f0beb4ca8e2597ba480971e60b9582e3da13ab05
thumbimigwe <thumbimigwe@gmail.com>
2016-04-12 06:45:44 +0300

    12th april

--- src/todoListApp/admin.py
+++ src/todoListApp/admin.py
@@ -4,7 +4,9 @@ from.models import toDo
 
 # Register your models here.
 
+@admin.register(toDo)
 class toDoAdmin(admin.ModelAdmin):
     list_display = ("description", "done")
+    list_editable = ('done',)
 
-admin.site.register(toDo, toDoAdmin)
+#admin.site.register(toDo, toDoAdmin)
